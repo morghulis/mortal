ee0073d2734a492ec80308f96c247528a0025edc
morghulis <morghulis@126.com>
2015-06-26 16:52:23 +0800

    add clear into ContextDocker

--- lib/python/2/xlibs/context.py
+++ lib/python/2/xlibs/context.py
@@ -3,15 +3,23 @@
 class ContextDocker(object):
 
     def __init__(self, **kwargs):
+        self.__dict__['default_kvs'] = kwargs
+
         self.__dict__['varspace'] = {}
         for k in kwargs.keys():
             self.__dict__['varspace'][k] = kwargs[k]
-            # print 'ctx.', k, kwargs[k]
 
     def docker(self, method):
         def wrapper(*args, **kwargs):
             return method(*args, **kwargs)
         return wrapper
+
+    def clear(self):
+        keys = self.__dict__['varspace'].keys()
+        for k in keys:
+            if k in self.__dict__['default_kvs']:
+                continue
+            self.__dict__['varspace'].pop(k)
 
 
     def __setattr__(self, name, value):
